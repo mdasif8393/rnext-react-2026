e6551fe6c6e4ba39c97d1eb8c0dbc5726bb548ff
mdasif8393 <md.asif.8393@gmail.com>
2026-07-22 10:14:31 +0600

    s

--- module-1-describing-the-ui/1.7-jsx-with-curly-braces/src/App.jsx
+++ module-1-describing-the-ui/1.7-jsx-with-curly-braces/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <ul style={color === "green" ? style2 : style1}>
       <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
+      <li>Prepare aeronautics lecturess</li>
       <li>Work on the alcohol-fuelled engine</li>
     </ul>
   );
