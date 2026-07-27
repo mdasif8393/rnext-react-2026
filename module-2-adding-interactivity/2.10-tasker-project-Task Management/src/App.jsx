44f2fe10fa440556d9d168da1d9086c79489f77b
mdasif8393 <md.asif.8393@gmail.com>
2026-07-27 12:08:27 +0600

    done header footer Hero

--- module-2-adding-interactivity/2.10-tasker-project-Task Management/src/App.jsx
+++ module-2-adding-interactivity/2.10-tasker-project-Task Management/src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <HeroSection />
+        <Footer />
       </div>
     </>
   );
--- module-2-adding-interactivity/2.10-tasker-project-Task Management/src/Header.jsx
+++ module-2-adding-interactivity/2.10-tasker-project-Task Management/src/Header.jsx
@@ -1,35 +1,11 @@
-import LWSLogo from "./assets/lws_logo.png";
+import LWSLogo from "./assets/lws-logo-en.svg";
 export default function Header() {
   return (
-    <nav className="py-4 md:py-6 border-b">
+    <nav className="py-6 md:py-8 fixed top-0 w-full bg-[#191D26]! z-50">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
-          <img
-            className="max-w-[100px] md:max-w-[165px]"
-            src={LWSLogo}
-            alt="Lws"
-          />
+          <img className="h-11.25" src={LWSLogo} alt="Lws" />
         </a>
-
-        <div className="flex gap-4 items-center">
-          <div className="relative">
-            {/* <button className="flex items-center gap-2">
-              <img className="max-w-8" src="./assets/lws_logo.png" alt="bangla" />
-              Bangla
-            </button> */}
-
-            {/* <div className="absolute right-0 top-full mt-2 w-40 rounded-md bg-white p-2 z-10 shadow-lg">
-            <li className="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-gray-100">
-              <img className="max-w-8" src="./assets/images/bd.png" alt="bangla" />
-              Bangla
-            </li>
-            <li className="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-gray-100">
-              <img className="max-w-8" src="./assets/images/usa.png" alt="bangla" />
-              English
-            </li>
-          </div> */}
-          </div>
-        </div>
       </div>
     </nav>
   );
