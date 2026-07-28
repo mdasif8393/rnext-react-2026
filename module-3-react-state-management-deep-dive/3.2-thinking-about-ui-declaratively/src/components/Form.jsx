ccf837e26fb357e2b7d72385f891f08f00c543d2
mdasif8393 <md.asif.8393@gmail.com>
2026-07-28 12:18:43 +0600

    reacting to input with state

--- module-3-react-state-management-deep-dive/3.2-thinking-about-ui-declaratively/src/components/Form.jsx
+++ module-3-react-state-management-deep-dive/3.2-thinking-about-ui-declaratively/src/components/Form.jsx
@@ -1,35 +1,64 @@
 import { useState } from "react";
 
-const Form = () => {
-  // mandatory data state
+export default function Form() {
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
 
-  //handlers
-  const handleTextChange = (e) => {
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+      setAnswer("");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setError(null);
     setAnswer(e.target.value);
-  };
-
-  if (status === "success")
-    return (
-      <div>
-        <h1>That's right!</h1>
-      </div>
-    );
+  }
 
   return (
-    <div>
+    <>
       <h2>City quiz</h2>
-      <p>What city is located on two continents?</p>
-      <form>
-        <textarea value={answer} onChange={handleTextChange}></textarea>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
         <br />
-        <button>Submit</button>
-        <p>Loading...</p>
-        <p className="Error">There was an error</p>
+        <button disabled={answer.length === 0 || status === "submitting"}>
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error.message}</p>}
       </form>
-    </div>
+    </>
   );
-};
+}
 
-export default Form;
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "dhaka";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
