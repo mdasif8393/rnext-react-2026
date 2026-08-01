1fe5c740aac0f52d8776a533ba769da6885b5d10
mdasif8393 <md.asif.8393@gmail.com>
2026-08-01 20:42:29 +0600

    DONE 3.12

---
+++ module-3-react-state-management-deep-dive/3.12-extracting-state-logic-into-a-reducer-project/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { initialTasks } from "./data/tasks";
+
+function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const getNextId = (data) => {
+    const maxId = data.reduce((prev, current) =>
+      prev && prev.id > current.id ? prev.id : current.id,
+    );
+    return maxId + 1;
+  };
+
+  //Handlers
+  const handleAddTask = (text) => {
+    setTasks([
+      ...tasks,
+      {
+        id: getNextId(tasks),
+        text,
+        done: false,
+      },
+    ]);
+  };
+
+  const handleChangeTask = (task) => {
+    const nextTasks = tasks.map((t) => {
+      if (t.id === task.id) {
+        return task;
+      } else {
+        return t;
+      }
+    });
+
+    setTasks(nextTasks);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
+  return (
+    <>
+      <h1>prague itinerary</h1>
+
+      <AddTask onAdd={handleAddTask} />
+
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+export default App;
