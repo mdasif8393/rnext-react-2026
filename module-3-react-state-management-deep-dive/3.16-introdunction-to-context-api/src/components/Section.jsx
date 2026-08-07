8240a66795c0ac19c52cebab22001fdd15d3ccd3
mdasif8393 <md.asif.8393@gmail.com>
2026-08-07 12:18:22 +0600

    3.16 Passing Data Deeply with Context - Introduction to Context API

--- module-3-react-state-management-deep-dive/3.16-introdunction-to-context-api/src/components/Section.jsx
+++ module-3-react-state-management-deep-dive/3.16-introdunction-to-context-api/src/components/Section.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { LevelContext } from "../contexts/LevelContext";
 
-function Section({ level, children }) {
+function Section({ children }) {
+  const level = useContext(LevelContext);
   return (
     <section className="section">
-      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
     </section>
   );
 }
