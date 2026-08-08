2154ce8d1293834d6a63f63b82b7ad5f0e557695
mdasif8393 <md.asif.8393@gmail.com>
2026-08-08 21:30:31 +0600

    add context api

--- module-3-react-state-management-deep-dive/3.21-cinerental-project/src/App.jsx
+++ module-3-react-state-management-deep-dive/3.21-cinerental-project/src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import MovieList from "./cine/MovieList";
+import { MovieContext } from "./context";
+import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./SideBar";
 
 function App() {
+  const [cartData, setCartData] = useState([]);
+
   return (
     <div>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-14">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-14">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </div>
   );
 }
