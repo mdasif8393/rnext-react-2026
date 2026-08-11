ea6392b18d40ebd80a2216aadd6da7515a953320
mdasif8393 <md.asif.8393@gmail.com>
2026-08-11 19:57:50 +0600

    4.4 Exposing a subset of the API with an imperative handle

--- module-4-part-1-escape-hatches/4.2-manipulating-the-dom-with-ref-getting-a-ref-to-the-node/src/components/MyInput.tsx
+++ module-4-part-1-escape-hatches/4.2-manipulating-the-dom-with-ref-getting-a-ref-to-the-node/src/components/MyInput.tsx
@@ -1,7 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = forwardRef((props, ref) => {
-  return <input {...props} ref={ref} />;
+  const realInputRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      realInputRef.current.focus();
+    },
+  }));
+
+  return <input {...props} ref={realInputRef} />;
 });
 
 export default MyInput;
