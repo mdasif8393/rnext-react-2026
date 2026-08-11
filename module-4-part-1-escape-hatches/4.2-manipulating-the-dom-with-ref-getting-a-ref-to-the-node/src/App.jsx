02d69ffc6c9c57ea8f658d48babef789388bcc70
mdasif8393 <md.asif.8393@gmail.com>
2026-08-11 13:27:09 +0600

    add CatFriends component

---
+++ module-4-part-1-escape-hatches/4.2-manipulating-the-dom-with-ref-getting-a-ref-to-the-node/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import CatFriends from "./components/CatFriends";
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        height: "100dvh",
+      }}
+    >
+      <CatFriends />
+    </div>
+  );
+}
+
+export default App;
