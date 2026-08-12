4ca3bfabdf88a74b84db9dfb15ebf5d86873f4a0
mdasif8393 <md.asif.8393@gmail.com>
2026-08-12 16:26:10 +0600

    ex 2

---
+++ module-4-part-1-escape-hatches/4.9-handling-effects-firing-twice-in-development/src/App.jsx
@@ -0,0 +1,14 @@
+import Map from "./components/Map";
+import "./App.css";
+import Dialogue from "./components/Dialogue";
+
+function App() {
+  return (
+    <>
+      {/* <Map /> */}
+      <Dialogue />
+    </>
+  );
+}
+
+export default App;
