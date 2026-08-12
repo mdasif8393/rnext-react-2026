db6e2f9b7f911a47800ce32e3dd4691bd3be737e
mdasif8393 <md.asif.8393@gmail.com>
2026-08-12 13:37:45 +0600

    4.8 Synchronizing with Effects - How to write an Effect

--- module-4-part-1-escape-hatches/4.8-how-to-write-an-effect/src/App.jsx
+++ module-4-part-1-escape-hatches/4.8-how-to-write-an-effect/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
-import MyComponent from "./components/MyComponent";
+// import MyComponent from "./components/MyComponent";
+import ChatRoom from "./utils/ChatRoom";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div
       style={{
@@ -9,7 +12,10 @@ function App() {
         height: "100dvh",
       }}
     >
-      <MyComponent />
+      <button onClick={() => setShow((s) => !s)}>
+        {show ? "Hide" : "Show"}
+      </button>
+      {show && <ChatRoom />}
     </div>
   );
 }
---
+++ module-4-part-1-escape-hatches/4.8-how-to-write-an-effect/src/utils/ChatRoom.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { createConnection } from "./chat";
+
+export default function ChatRoom() {
+  useEffect(() => {
+    const connection = createConnection();
+    connection.connect();
+
+    return () => {
+      connection.disconnect();
+    };
+  }, []);
+  return <h1>Welcome to the chat!</h1>;
+}
