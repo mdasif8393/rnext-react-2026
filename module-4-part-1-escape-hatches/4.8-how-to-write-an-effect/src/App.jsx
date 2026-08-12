1a25d222ae8f796c75621e71a340e5f7363a972d
mdasif8393 <md.asif.8393@gmail.com>
2026-08-12 13:01:54 +0600

    useEfect

---
+++ module-4-part-1-escape-hatches/4.8-how-to-write-an-effect/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        height: "100dvh",
+      }}
+    >
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
