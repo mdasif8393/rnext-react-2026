8b086df7036362ab76a19bec072c09dec1878778
mdasif8393 <md.asif.8393@gmail.com>
2026-08-13 11:17:43 +0600

    example ২

--- module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
+++ module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
-import Example1 from "./components/Example1";
+
+import Example2 from "./components/Example2";
 
 function App() {
+  const [userId, setUserId] = useState(1);
   return (
     <>
-      <Example1 />
+      <div>
+        <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
+          Switch to Profile No. {userId === 1 ? 2 : 1}
+        </button>
+      </div>
+      <Example2 key={userId} userId={userId} />
     </>
   );
 }
