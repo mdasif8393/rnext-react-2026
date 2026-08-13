a785467e48f0b5d6e05ecfe93babc3fb7a64acb3
mdasif8393 <md.asif.8393@gmail.com>
2026-08-13 11:23:51 +0600

    example ২

--- module-4-part-1-escape-hatches/4.9-handling-effects-firing-twice-in-development/src/App.jsx
+++ module-4-part-1-escape-hatches/4.9-handling-effects-firing-twice-in-development/src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-import Comments from "./components/Comments";
+import Playground from "./components/Playground";
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <>
       {/* <Map /> */}
       {/* <Dialogue /> */}
       {/* <Scroller /> */}
       {/* <Animation /> */}
-      <Comments postId="2" />
+      {/* <Comments postId="2" /> */}
+      <button onClick={() => setShow(!show)}>
+        {show ? "Unmount" : "Mount"} the component
+      </button>
+      {show && <hr />}
+      {show && <Playground />}
     </>
   );
 }
