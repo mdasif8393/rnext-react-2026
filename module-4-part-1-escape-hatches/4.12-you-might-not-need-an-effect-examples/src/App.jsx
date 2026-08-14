9f2d2b0cb2194005f8aad2d240e222af0d259f82
mdasif8393 <md.asif.8393@gmail.com>
2026-08-14 12:33:29 +0600

    example 5

--- module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
+++ module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Example4 from "./components/Example4";
+import Example5 from "./components/Example5";
 
 function App() {
   return (
     <>
-      <Example4 />
+      <Example5 />
     </>
   );
 }
