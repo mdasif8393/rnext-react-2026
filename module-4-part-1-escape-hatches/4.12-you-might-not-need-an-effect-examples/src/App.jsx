9401e5faf4b897178758e643b9fd6e42f6275a77
mdasif8393 <md.asif.8393@gmail.com>
2026-08-14 16:55:23 +0600

    example 7

--- module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
+++ module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import { useState } from "react";
-import Example7 from "./components/Example7";
+
+import Example9 from "./components/Example9";
 
 function App() {
-  const [isOn, setIsOn] = useState(false); // example 7
   return (
     <>
-      <p>{isOn ? "On" : "Off"}</p>
-      <Example7 onChange={setIsOn} />
+      <Example9 />
     </>
   );
 }
