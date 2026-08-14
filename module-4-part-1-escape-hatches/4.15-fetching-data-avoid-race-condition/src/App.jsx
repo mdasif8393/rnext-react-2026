83f4406cdc113396dfef4be5ed77e8aa008e349a
mdasif8393 <md.asif.8393@gmail.com>
2026-08-14 18:48:25 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M

---
+++ module-4-part-1-escape-hatches/4.15-fetching-data-avoid-race-condition/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchResults from "./components/SearchResults";
+
+function App() {
+  return (
+    <>
+      <SearchResults />
+    </>
+  );
+}
+
+export default App;
---
+++ module-4-part-1-escape-hatches/4.15-fetching-data-avoid-race-condition/src/utils/fetchResuls.js
@@ -0,0 +1,18 @@
+import { getRndInteger } from "./getRandoInteger";
+
+
+export default function fetchResults(query, page) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            if (query !== "") {
+                if (page % 2 === 0) {
+                    resolve(`Even-${query}`);
+                } else {
+                    resolve(`Odd-${query}`);
+                }
+            } else {
+                resolve();
+            }
+        }, getRndInteger(1000, 5000));
+    });
+}
