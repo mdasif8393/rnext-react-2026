d71cb5cc715255b37a4bcd392ca4a29474fc1c51
mdasif8393 <md.asif.8393@gmail.com>
2026-08-14 12:04:45 +0600

    example 4

--- module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
+++ module-4-part-1-escape-hatches/4.12-you-might-not-need-an-effect-examples/src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-
-import Example3 from "./components/Example3";
-import { items1, items2 } from "./data/itemsData";
+import Example4 from "./components/Example4";
 
 function App() {
-  // const [userId, setUserId] = useState(1);
-  const [items, setItems] = useState(items1);
-
   return (
     <>
-      {/* <div>
-        <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
-          Switch to Profile No. {userId === 1 ? 2 : 1}
-        </button>
-      </div>
-      <Example2 key={userId} userId={userId} /> */}
-      <button onClick={() => setItems(items2)}>Switch to Items2</button>{" "}
-      <button onClick={() => setItems(items1)}>Switch to Items1</button>
-      <Example3 items={items} />
+      <Example4 />
     </>
   );
 }
